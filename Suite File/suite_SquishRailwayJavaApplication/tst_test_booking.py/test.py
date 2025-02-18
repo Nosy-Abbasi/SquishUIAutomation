341018ba6cbd8e30a27c01e3992ed4465d79204f
unknown <noshadam@spanidea.com>
2025-02-18 15:35:30 +0530

    Allure report work

--- Suite File/suite_SquishRailwayJavaApplication/tst_test_booking.py/test.py
+++ Suite File/suite_SquishRailwayJavaApplication/tst_test_booking.py/test.py
@@ -1,5 +1,7 @@
 import json
 import names
+import allure
+import pytest
 
 source(findFile("scripts", "BookTicket.py"))
 source(findFile("scripts", "CommonFunctions.py"))
@@ -7,29 +9,27 @@ source(findFile("scripts", "GVariables.py"))
 
 
 def main():
-    
+
+    allure.suite("Booking Functionlity")
+
     data = data_load(BookTicketJSON)
-    
 
+    
     def test_booking_submit():
-        
         for ticket in data["BookTicket"]:
+                launch_application(Railway)  
+                fill_booking_form(ticket)
+                Save_Booking(ticket)
             
-            # Execute actions
-            launch_application(Railway)  
-            fill_booking_form(ticket)
-            Save_Booking(ticket)
-            
-    def test_cancel_button():    
-        for ticket1 in data["CancleButton"]:
-            
-            # Execute actions
-            launch_application(Railway)
-            fill_booking_form(ticket1)
-            Cancel_button_Working()  
     
-    test_booking_submit()
-    test_cancel_button()
-            
+    
+    def test_cancel_button():
+        for ticket1 in data["CancleButton"]:
+                launch_application(Railway)
+                fill_booking_form(ticket1)
+                Cancel_button_Working()  
 
-         
+    allure.testcase("Test101", "Booking Submit")
+    test_booking_submit()
+    allure.testcase("Test102", "Booking Cancel")
+    test_cancel_button()
